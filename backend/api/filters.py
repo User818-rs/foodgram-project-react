5cc38bef7e8f41cf86e8e749ba3ea19ee40290ad
User818-rs <121470460+User818-rs@users.noreply.github.com>
2024-01-17 11:57:18 +0400

    Fin

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -18,6 +18,17 @@ class IngredientFilter(FilterSet):
         fields = ["name"]
 
 
+class IngredientFilter(FilterSet):
+    """
+    Фильтр для поиска ингредиентов по имени.
+    """
+    name = CharFilter(lookup_expr="istartswith")
+
+    class Meta:
+        model = Ingredient
+        fields = ["name"]
+
+
 class RecipeFilter(FilterSet):
     """
     Фильтр для поиска рецептов с возможностью фильтрации по автору, тегам,
@@ -37,12 +48,8 @@ class RecipeFilter(FilterSet):
         model = Recipe
         fields = ("tags", "author", "is_favorited", "is_in_shopping_cart")
 
-    def get_favorite(self, queryset, name, item_value):
-        if self.request.user.is_authenticated and item_value:
-            queryset = queryset.filter(in_favorite__user=self.request.user)
-        return queryset
+    def get_is_favorited(self, queryset, name, value):
+        return queryset.filter(fav_recipes__user=self.request.user)
 
-    def get_shopping(self, queryset, name, item_value):
-        if self.request.user.is_authenticated and item_value:
-            queryset = queryset.filter(shopping_cart__user=self.request.user)
-        return queryset
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        return queryset.filter(shopping_list__user=self.request.user)
