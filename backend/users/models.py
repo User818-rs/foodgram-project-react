ede5b6690ee3357b43040a83e89575b69d6c8871
Stanislav <akacukee@mail.ru>
2024-01-30 21:00:42 +0400

    после ревью

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 from api.validators import validate_me, validate_symbols_in_fields
 
